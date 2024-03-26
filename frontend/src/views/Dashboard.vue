c7b1da555b5d2a004b25920d6339528345ea78e7
AlmazDefourten <almaz.sahabiev2004@mail.ru>
2024-03-26 19:46:14 +0300

    test ci/cd frontend

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -18,7 +18,7 @@
           </stats-card>
         </b-col>
         <b-col xl="3" md="6">
-          <stats-card title="аааа уssdsdфффыыы"
+          <stats-card title="ааааdddddd уssdsdфффыыы"
                       type="gradient-orange"
                       sub-title="2,356"
                       icon="ni ni-chart-pie-35"
