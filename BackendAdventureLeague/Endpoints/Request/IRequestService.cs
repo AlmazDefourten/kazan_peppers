5814151382b119ec3b5ab028b1d22807aa617e9e
AlmazDefourten <almaz.sahabiev2004@mail.ru>
2024-03-29 02:21:00 +0300

    interface

---
+++ BackendAdventureLeague/Endpoints/Request/IRequestService.cs
@@ -0,0 +1,12 @@
+﻿namespace BackendAdventureLeague.Endpoints.Request;
+
+public interface IRequestService
+{
+    Task CreateAsync(Models.Request account, CancellationToken cancellationToken = default);
+    
+    Task<IList<Models.Request>> ListAsync(CancellationToken cancellationToken = default);
+    
+    Task<Models.Request?> GetAsync(long id, CancellationToken cancellationToken = default);
+    
+    Task DeleteAsync(long id, CancellationToken cancellationToken = default);
+}
