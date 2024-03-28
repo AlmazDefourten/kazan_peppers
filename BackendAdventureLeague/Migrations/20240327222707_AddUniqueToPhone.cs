af83f22606e014d59eac9fb2214a9793323a29b9
AlmazDefourten <almaz.sahabiev2004@mail.ru>
2024-03-28 05:01:48 +0300

    migrations

---
+++ BackendAdventureLeague/Migrations/20240327222707_AddUniqueToPhone.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendAdventureLeague.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueToPhone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_Phone",
+                table: "AspNetUsers",
+                column: "Phone",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_Phone",
+                table: "AspNetUsers");
+        }
+    }
+}
