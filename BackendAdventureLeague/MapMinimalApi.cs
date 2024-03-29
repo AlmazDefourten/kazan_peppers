9a6139885b8689430c5d410780101a4087b36edb
AlmazDefourten <almaz.sahabiev2004@mail.ru>
2024-03-29 03:58:17 +0300

    authorized only add

--- BackendAdventureLeague/MapMinimalApi.cs
+++ BackendAdventureLeague/MapMinimalApi.cs
@@ -11,12 +11,14 @@ public static class MapMinimalApi
     public static void DoMap(WebApplication app)
     {
         app.MapGroup("account")
+            .RequireAuthorization()
             .MapGet(async ([FromServices]IAccountCrudEndpoints service, long id) => await service.GetAsync(id), "get")
             .MapPost(async ([FromServices]IAccountCrudEndpoints service, Account account) => await service.CreateAsync(account), "create")
             .MapDelete(async ([FromServices]IAccountCrudEndpoints service, long id) => await service.DeleteAsync(id), "delete")
             .MapGet(async ([FromServices]IAccountCrudEndpoints service) => await service.ListAsync(), "list");
         
         app.MapGroup("request")
+            .RequireAuthorization()
             .MapGet(async ([FromServices]IRequestService service, long id) => await service.GetAsync(id), "get")
             .MapPost(async ([FromServices]IRequestService service, Request request) => await service.CreateAsync(request), "create")
             .MapDelete(async ([FromServices]IRequestService service, long id) => await service.DeleteAsync(id), "delete")
