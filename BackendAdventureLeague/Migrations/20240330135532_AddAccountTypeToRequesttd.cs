5d4bde31ff3068e9421fd3d6d2f60c1c4c96c126
AlmazDefourten <almaz.sahabiev2004@mail.ru>
2024-03-30 16:55:49 +0300

    hhh

---
+++ BackendAdventureLeague/Migrations/20240330135532_AddAccountTypeToRequesttd.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendAdventureLeague.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountTypeToRequesttd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "CostToBy",
+                table: "Requests",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CostToBy",
+                table: "Requests");
+        }
+    }
+}
