7f83f11766ab124883a52d6e2dbb236d9222c70f
AlmazDefourten <almaz.sahabiev2004@mail.ru>
2024-03-30 17:01:09 +0300

    create notificanos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,26 +5,28 @@
 <script>
 
 import axios from "axios";
+import Notifications from "@/components/NotificationPlugin/Notifications.vue";
 
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
-// axios.interceptors.request.use(function (config) {
-//   console.log(localStorage.getItem("accessToken"));
-//   config.headers.Authorization = "Bearer " + localStorage.getItem("accessToken");
-//
-//   return config;
-// });
-//
-// axios.interceptors.response.use((response) => {
-//   return response;
-// }, (error) => { // Anything except 2XX goes to here
-//   console.log(error);
-//   if (error.message == "Network Error") {
-//     window.location = window.location.protocol + "//" + window.location.host + "/#/login"
-//   } else {
-//     return Promise.reject(error); // Delegate error to calling side
-//   }
-// });
+ axios.interceptors.request.use(function (config) {
+   console.log(localStorage.getItem("accessToken"));
+   config.headers.Authorization = "Bearer " + localStorage.getItem("accessToken");
+
+   return config;
+ });
+
+ axios.interceptors.response.use((response) => {
+   return response;
+ }, (error) => {
+   console.log(error);
+   if (error.message == "Network Error") {
+     window.location = window.location.protocol + "" + window.location.host + "/#/login"
+   } else {
+     return Promise.reject(error);
+   }
+ });
 export default {
+  components: {Notifications}
 }
 </script>
--- frontend/src/views/NewAccount.vue
+++ frontend/src/views/NewAccount.vue
@@ -17,7 +17,9 @@ export default {
       axios.post("http://107.173.25.219:81/account/create", this.model)
         .then(response => {
           this.$emit('close');
+          this.$notify({type: "success", icon: "mdi mdi-check-bold", verticalAlign: 'top', horizontalAlign: 'right', message: 'Счет успешно создан'});
         }, error => {
+          this.$notify({type: "danger", icon: "mdi mdi-remove", verticalAlign: 'top', horizontalAlign: 'right', message: 'Не удалось создать акаунт'});
           console.log(error);
         });
     },
@@ -33,8 +35,8 @@ export default {
     <b-dropdown id="dropdown-1" class="m-md-2">
       <template #button-content> {{ selectedCurrency || '-----' }} </template>
       <b-dropdown-item @click="() =>{model.currencyType= 1; selectedCurrency='Рубли';}">Рубли (₽)</b-dropdown-item>
-      <b-dropdown-item @click="() =>{model.currencyType= 1; selectedCurrency='Юани';}">Юани (¥)</b-dropdown-item>
-      <b-dropdown-item @click="() =>{model.currencyType= 1; selectedCurrency='Дирхам';}">>Дирхам (د.إ)</b-dropdown-item>
+      <b-dropdown-item @click="() =>{model.currencyType= 2; selectedCurrency='Юани';}">Юани (¥)</b-dropdown-item>
+      <b-dropdown-item @click="() =>{model.currencyType= 3; selectedCurrency='Дирхам';}">>Дирхам (د.إ)</b-dropdown-item>
     </b-dropdown>
   <base-input
     type="text"
