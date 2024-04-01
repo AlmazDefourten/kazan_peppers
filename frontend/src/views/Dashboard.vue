66013458aa21094d11795edf2a749c55901ecaba
sergey2k161 <joysergey4@gmail.com>
2024-04-02 00:03:01 +0300

    Дизайн

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,5 +1,28 @@
 <template>
 <div>
+  <link href="/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <b-modal ref="listRequest" :hide-footer="true" id="modal-5" title="Список заявок">
+    <div role="tablist" class="accordion">
+      <b-card v-for="(data, index) in requestList" :key="index" no-body class="mb-1">
+        <b-card-header header-tag="header" role="tab" :v-b-toggle="'accordion-' + index" class="d-flex justify-content-between">
+          <h5 class="mb-0">Заявка №{{ data.id }}</h5>
+          <base-button id="inter-account-transfer" v-b-modal.modal-5 style="background-color: gray; width: 40px; display: flex; justify-content: center; align-items: center;">
+            <i class="fas fa-times"></i>
+          </base-button>
+        </b-card-header>
+        <b-collapse :id="'accordion-' + index" visible accordion="my-accordion" role="tabpanel">
+          <b-card-body>
+            <b-card-text>
+              На сумму: {{ data.amountToBuy }}<br>
+              Цена: {{ data.costToBy }}<br>
+              Счета для оплаты {{ data.accountFrom }}, Счет пополнения {{ data.accountTo }}
+            </b-card-text>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
+    </div>
+  </b-modal>
+
   <div class="h-100 w-100 d-flex">
     <b-modal ref="transferModal" :hide-footer="true" id="modal-4" title="Перевод между счетами" >
       <template slot="default">
@@ -116,8 +139,9 @@
       </div>
     </div>
     <!--      ЗАЯВКА-->
-    <div class="pb-2 pt-5 pt-md-8">
-      <b-button id="create-order-button" class="m-md-2" style="width: 259px; border-color: #1fa2ff" v-b-modal.modal-1> Заявка </b-button>
+    <div class="pb-2 pt-5 pt-md-8 pr-0">
+      <b-button id="create-order-button" class="m-md-2" style="width: 150px; border-color: #1fa2ff" v-b-modal.modal-1> Заявка </b-button>
+      <b-button @click="loadRequestList" id="listRequest" class="m-md-2" v-b-modal.modal-5  style="width: 150px; border-color: #1fa2ff">Список заявок</b-button>
       <b-modal :hide-footer="true" id="modal-1" title="Создание заявки">
         <template slot="default">
           <label for="accountFrom"class="col-md-12 col-form-label form-control-label">На:</label>
@@ -186,7 +210,7 @@
 <!--      ОТОБРАЖЕНИЕ СЧЕТОВ-->
       <div type="hbox">
         <!-- Если у пользователя есть счета, отображаем первый счет -->
-        <b-button v-b-toggle.collapse-1 class="m-2 my-button" v-if="accounts.length > 0">
+        <b-button v-b-toggle.collapse-1 class="m-2 my-button" v-if="accounts.length > 0" style="width: 250px">
           {{ accounts[0].name }}, {{ getCurrencyTypeString(accounts[0].currencyType) }}, {{ accounts[0].sum }}
         </b-button>
 
@@ -194,14 +218,15 @@
         <b-button v-b-toggle.collapse-2 class="m-1 my-button" v-else>
           Toggle Collapse
         </b-button>
+        <b-button id="create-account-bottom" class="m-2 my-button" v-b-modal.modal-2 > + </b-button>
 
         <!-- Element to collapse -->
-        <b-collapse v-for="(data, index) in accounts" :key="index" id="collapse-1">
+        <b-collapse v-for="(data, index) in accounts" :key="index" id="collapse-1" style="padding-left: 8px; padding-bottom: -1px">
           <stats-card :title="data.name"
                       type="gradient-info"
                       :sub-title="data.sum + ' ' + getCurrencyTypeString(data.currencyType)"
                       class="mb-4 w-25"
-                      style="min-width: 300px"
+                      style="min-width: 250px;"
           >
             <template slot="icon">
               <base-button id="inter-account-transfer" v-b-modal.modal-4 size="sm" style="background-color: #003791" icon class="w-100 h-100">
@@ -211,7 +236,7 @@
           </stats-card>
         </b-collapse>
 
-        <b-button id="create-account-bottom" class="m-2 my-button" v-b-modal.modal-2 > + </b-button>
+
         <b-modal ref="accountModal" :hide-footer="true" id="modal-2" title="Открытие нового счета" >
           <template slot="default">
             <new-account @close="() => { this.$refs.accountModal.hide();  loadAccounts()}"> </new-account>
@@ -220,25 +245,13 @@
       </div>
     </div>
 <!--    Список заявок-->
-    <div class="pb-2 pt-5 pt-md-8">
-      <b-button id="listRequest" class="m-2 my-button" v-b-modal.modal-5> -><- </b-button>
-      <div class="d-flex pt-2 justify-content-start">
-        <b-modal ref="listRequest" :hide-footer="true" id="modal-5" title="Список заявок">
-          <b-collapse v-for="(data, index) in requestList" :key="index">
-            <template slot="default">
-              <stats-card :title="data.id"
-                          type="mdi"
-                          :sub-title="data.expirationTime"
-              >
-              </stats-card>
-            </template>
-          </b-collapse>
-        </b-modal>
-      </div>
-
-    </div>
+<!--    <div class="pb-2 pt-5 pt-md-8">-->
+<!--&lt;!&ndash;      <b-button id="create-order-button" class="m-md-2" style="width: 259px; border-color: #1fa2ff" v-b-modal.modal-1> Заявка </b-button>&ndash;&gt;-->
+<!--      <b-button @click="loadRequestList" id="listRequest" class="m-md-2" v-b-modal.modal-5  style="width: 150px; border-color: #1fa2ff">Список заявок</b-button>-->
+<!--      <div class="d-flex pt-2 justify-content-start">-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
-
   </div>
 </template>
 
@@ -271,6 +284,7 @@
         accounts: [0, 1],
         yuanCurrency: 0,
         dirhamCurrency: 0,
+        requestList: []
       };
     },
     methods: {
@@ -291,7 +305,7 @@
             console.log(error);
           });
       },
-      // СДЕЛАТЬ!!!!!!!!!!!!
+
       async interAccountTransfer(){
         const modelToTransfer ={
           idTo: this.transferModel.selectedTo.id,
@@ -311,6 +325,15 @@
           .then(response => {
             this.accounts = response.data;
             console.log(response.data);
+          }, error => {
+            console.log(error);
+          });
+      },
+      async loadRequestList() {
+        await axios.get(ApiAddress + "/request/list")
+          .then(response => {
+            this.requestList = response.data;
+            console.log(this.requestList[0]);
           }, error => {
             console.log(error);
           });
